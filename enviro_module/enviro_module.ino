ae55910880b8d65dad46e715dc80d7a6c2347fd2
Jack <schoenjack98@gmail.com>
2024-03-28 13:18:09 -0400

    collecting libraries

--- enviro_module/enviro_module.ino
+++ enviro_module/enviro_module.ino
@@ -1,9 +1,13 @@
-#include <CD74HC4067.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_BMP280.h>
-#include <AnalogUVSensor.h>
-#include <Adafruit_AHTX0.h
+#include <CD74HC4067.h> // mux
+#include <GyverOLED.h> // 128x32 oled
+#include <Wire.h> // many
+#include <SPI.h> // many
+#include <Adafruit_BMP280.h> // pressure sensor (HW-611)
+#include <AnalogUVSensor.h> // UV (obv)
+#include <Adafruit_AHTX0.h> // two temp/humidity sensors
+#include <DFRobot_ENS160.h> // gas sensor
+#include <Adafruit_Sensor.h> // compass
+#include <Adafruit_HMC5883_U.h> // compass
 
 void setup() {
   // put your setup code here, to run once:
