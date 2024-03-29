a27b63f967d153602e0bdff2bca7fc1639c13c59
Jack <schoenjack98@gmail.com>
2024-03-29 09:33:25 -0400

    finally able to interface
    thank you Gregor Christandl for having a library that helped me interface with my device!

--- enviro_module/enviro_module.ino
+++ enviro_module/enviro_module.ino
@@ -1,20 +1,87 @@
 #include <CD74HC4067.h> // mux
 #include <GyverOLED.h> // 128x32 oled
-#include <Wire.h> // many
-#include <SPI.h> // many
-#include <Adafruit_BMP280.h> // pressure sensor (HW-611)
+#include <Wire.h> // I2C
+#include <SPI.h> // serial peripherals
+#include <Arduino.h>
+#include <Wire.h>
+#include <BMx280I2C.h> // temperature and pressure
+// example code and library (I'm guessing) is copyright (c) 2018 Gregor Christandl
+// this is currently his code with my libs added
+// I'm in the process of rewriting it for my purpose
+// credit where it is due!
 #include <AnalogUVSensor.h> // UV (obv)
 #include <Adafruit_AHTX0.h> // two temp/humidity sensors
 #include <DFRobot_ENS160.h> // gas sensor
 #include <Adafruit_Sensor.h> // compass
 #include <Adafruit_HMC5883_U.h> // compass
 
+#define I2C_ADDRESS 0x76
+
+//create a BMx280I2C object using the I2C interface with I2C Address 0x76
+BMx280I2C bmx280(I2C_ADDRESS);
+
 void setup() {
   // put your setup code here, to run once:
+	Serial.begin(9600);
 
+	//wait for serial connection to open (only necessary on some boards)
+	while (!Serial);
+
+	Wire.begin();
+
+	//begin() checks the Interface, reads the sensor ID (to differentiate between BMP280 and BME280)
+	//and reads compensation parameters.
+	if (!bmx280.begin())
+	{
+		Serial.println("begin() failed. check your BMx280 Interface and I2C Address.");
+		while (1);
+	}
+
+	if (bmx280.isBME280())
+		Serial.println("sensor is a BME280");
+	else
+		Serial.println("sensor is a BMP280");
+
+	//reset sensor to default parameters.
+	bmx280.resetToDefaults();
+
+	//by default sensing is disabled and must be enabled by setting a non-zero
+	//oversampling setting.
+	//set an oversampling setting for pressure and temperature measurements. 
+	bmx280.writeOversamplingPressure(BMx280MI::OSRS_P_x16);
+	bmx280.writeOversamplingTemperature(BMx280MI::OSRS_T_x16);
+
+	//if sensor is a BME280, set an oversampling setting for humidity measurements.
+	if (bmx280.isBME280())
+		bmx280.writeOversamplingHumidity(BMx280MI::OSRS_H_x16);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+	delay(1000);
+
+	//start a measurement
+	if (!bmx280.measure())
+	{
+		Serial.println("could not start measurement, is a measurement already running?");
+		return;
+	}
+
+	//wait for the measurement to finish
+	do
+	{
+		delay(100);
+	} while (!bmx280.hasValue());
+
+	Serial.print("Pressure: "); Serial.println(bmx280.getPressure());
+	Serial.print("Temperature: "); Serial.println(bmx280.getTemperature());
+
+	//important: measurement data is read from the sensor in function hasValue() only. 
+	//make sure to call get*() functions only after hasValue() has returned true. 
+	if (bmx280.isBME280())
+	{
+		Serial.print("Humidity: "); 
+		Serial.println(bmx280.getHumidity());
+	}
 }
